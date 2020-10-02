e060f4284155f4b70d804ec5516018cc9eca41f3
Liza_silent <elizavetad@ya.ru>
2020-10-02 12:17:34 +0300

    проверка гита

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,36 +1,34 @@
 export default class Popup {
-        constructor (popupSelector) {
-        this._popup = document.querySelector(popupSelector);
-        this._handleEscClose = this._handleEscClose.bind(this)
-        }
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
 
-        open() {
-            this._popup.classList.add("popup_is-open");
-            document.body.addEventListener("keydown", this._handleEscClose);
-        };
+  open() {
+    this._popup.classList.add("popup_is-open");
+    document.body.addEventListener("keydown", this._handleEscClose);
+  }
 
-        close() {
-            this._popup.classList.remove("popup_is-open");
-            document.body.addEventListener("keydown", this._handleEscClose);
-        };
+  close() {
+    this._popup.classList.remove("popup_is-open");
+    document.body.addEventListener("keydown", this._handleEscClose);
+  }
 
-        _handleEscClose(evt) {
-            if (evt.key === "Escape") {
-            this.close();
-        }
-            };
+  _handleEscClose(evt) {
+    if (evt.key === "Esc") {
+      this.close();
+    }
+  }
 
+  setEventListeners() {
+    this._popup
+      .querySelector(".popup__close-btn")
+      .addEventListener("click", () => this.close());
 
-
-        setEventListeners() {
-
-          this._popup.querySelector(".popup__close-btn").addEventListener("click", () => this.close());
-
-            this._popup.addEventListener("click", (evt) => {
-                if (evt.target.classList.contains("popup")) {
-                    this.close();
-                }
-            });
-        }
+    this._popup.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("popup")) {
+        this.close();
+      }
+    });
+  }
 }
-
