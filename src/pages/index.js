c39da3b9fd3b1327f317a99f8d3ac5d866526d4f
lizasilent <elizavetad@yandex.ru>
2020-10-04 12:46:24 +0300

    организация файлов

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import {initialCards, settings} from "./initials.js";
-import Card from "./Card.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import FormValidator from "./FormValidator.js";
-import UserInfo from "./UserInfo.js";
+import {initialCards, settings} from "../components/initials.js";
+import Card from "../components/Card.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import FormValidator from "../components/FormValidator.js";
+import UserInfo from "../components/UserInfo.js";
 
 
 const editProfileButton = document.querySelector(".profile__edit-btn");
