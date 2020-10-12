35d6b2643a507c6b0772a3761f38231ba20a8ccc
lizasilent <elizavetad@yandex.ru>
2020-10-12 23:10:11 +0300

    начата работа с апи

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import "./pages/index.css";
 import { initialCards, settings } from "./components/initials.js";
-// import Api from "./components/Api.js"
-import Card from "./components/Card.js";
+import Api from "./components/Api.js";
 import Section from "./components/Section.js";
+import Card from "./components/Card.js";
+import UserInfo from "./components/UserInfo.js";
 import PopupWithImage from "./components/PopupWithImage.js";
 import PopupWithForm from "./components/PopupWithForm.js";
+import PopupWithSubmit from "./components/PopupWithSubmit";
 import FormValidator from "./components/FormValidator.js";
-import UserInfo from "./components/UserInfo.js";
-// import PopupWithSubmit from "./components/PopupWithSubmit";
-
 
 const editProfileButton = document.querySelector(".profile__edit-btn");
 const addCardButton = document.querySelector(".profile__add-btn");
 const list = document.querySelector(".grid__template");
 
-// const api = new Api({
-// baseUrl: "https://mesto.nomoreparties.co/v1/cohort-16",
-// headers: {
-//   authorization: '9a08681-995b-4f4f-93e1-9a39f0f490cc',
-//   "content-type": "application/json"
-// }
-
-// })
+const api = new Api({
+baseUrl: "https://mesto.nomoreparties.co/v1/cohort-16",
+headers: {
+  authorization: '9a08681-995b-4f4f-93e1-9a39f0f490cc',
+  "Content-Type": "application/json"
+}
+})
 
 
 const addCard = (item) => {
